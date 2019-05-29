51bfadfb14b2959c6c0156d4ba4a793f886a1920
christopherkalule <ckalule7@gmail.com>
2019-05-29 22:48:45 +0300

    fixture 11

---
+++ src/models/seeders/20190529192333-Messages.js
@@ -0,0 +1,64 @@
+"use strict";
+const faker = require("faker");
+const uuidv3 = require('uuid/v1');
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Messages",
+      [
+        {
+          id:1,
+          message: "mwambie deree acome na kamzigo",
+          status: "read",
+          senderId: 1,
+          receiverId: 2,
+          createdAt: faker.date.past(),
+          updatedAt: faker.date.past()
+        },
+        {
+          id:2,
+          message: "Jamaa kasema anachelewa so we tangulia tu",
+          status: "unread",
+          senderId: 2,
+          receiverId: 3,
+          createdAt: faker.date.past(),
+          updatedAt: faker.date.past()
+        },
+        {
+          id:3,
+          message: "mwambie deree acome na kamzigo",
+          status: "read",
+          senderId:3,
+          receiverId: 1,
+          createdAt: faker.date.past(),
+          updatedAt: faker.date.past()
+        },
+        {
+          id:4,
+          message: "mwambie deree acome na kamzigo",
+          status: "read",
+          senderId: 1,
+          receiverId:3,
+          createdAt: faker.date.past(),
+          updatedAt: faker.date.past()
+        },
+        {
+          id:5,
+          message: "mwambie deree acome na kamzigo",
+          status: "read",
+          senderId: 2,
+          receiverId:1,
+          createdAt: faker.date.past(),
+          updatedAt: faker.date.past()
+        },
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+   
+      Example:
+      return queryInterface.bulkDelete('Messages', null, {});
+  }
+};
